ed2656df4fb15453a8908d9f5b928eb8ea23dab6
Aileen Tran <aileen.tran27@gmail.com>
2020-01-26 17:33:51 -0800

    WIP psuedocode

--- lunch.py
+++ lunch.py
@@ -41,3 +41,37 @@ This starts at 4 (highest of the four middle), then goes right:
 15
 
 """
+
+# thoughts
+
+# row = garden[#]
+# col = garden[#][#]
+# garden[row][col]
+
+# WNES
+# west = left = garden[row][col-1]
+# north = up = garden[row-1][col]
+# east = right = garden[row][col+1]
+# south = down = garden[row+1][col]
+
+# to start
+# need to access middle of the matrix: middle row, middle columns
+# if even number, get two middle rows and/or two middle columns
+# if odd nrows and ncols // 2 + 1 (to get to middle thingy)
+# if even nrows and ncols 
+
+
+def lunch_count(garden):
+    """Given a garden of nrows of ncols, return carrots eaten."""
+
+    # Sanity check that garden is valid
+
+    row_lens = [len(row) for row in garden]
+    assert min(row_lens) == max(row_lens), "Garden not a matrix!"
+    assert all(all(type(c) is int for c in row) for row in garden), \
+        "Garden values must be ints!"
+
+    # Get number of rows and columns
+
+    nrows = len(garden)
+    ncols = len(garden[0])
