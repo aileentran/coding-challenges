cf5424abce1f9490210f9d2534fa91c8034075dd
Aileen Tran <aileen.tran27@gmail.com>
2020-02-03 16:58:42 -0800

    Created revlinkedlist file

---
+++ revlinkedlist.py
@@ -0,0 +1,11 @@
+"""Write a function that takes the head node of a linked list and returns the head of a new linked list, where the nodes are in the reverse order.
+
+Test case:
+>>> ll = Node(1, Node(2, Node(3)))
+>>> new_ll = reverse_linked_list(ll)
+>>> new_ll.as_string()
+'321
+
+"""
+
+# thoughts
