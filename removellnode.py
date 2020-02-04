9d74820472ae57ad47709ed264b6a4c5dfbc539f
Aileen Tran <aileen.tran27@gmail.com>
2020-02-03 16:22:25 -0800

    Renamed files and created removellnode

---
+++ removellnode.py
@@ -0,0 +1,11 @@
+"""
+We want a function that will delete a node from the series, so that everything moves over by one.
+
+Given a node in a linked list, remove it.
+
+Remove this node from a linked list. Note that we do not have access to
+any other nodes of the linked list, like the head or the tail.
+
+Does not return anything; changes list in place.
+"""
+
