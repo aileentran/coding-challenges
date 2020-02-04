8376082853747fb7ab67275db65d7c12c9760dae
Aileen Tran <aileen.tran27@gmail.com>
2020-02-03 17:05:01 -0800

    Set up Node class and as_string func

--- revlinkedlist.py
+++ revlinkedlist.py
@@ -9,3 +9,40 @@ Test case:
 """
 
 # thoughts
+# input: head of linked list
+# output: head of linked list (reversed)
+
+# create node class w/being able to print linked list as a string
+# self.data, self.next 
+# while loopin and etc. 
+
+# function takes in linked list
+# make an.. empty node? or set the node to the first value in linked list 
+
+# traverse through linked list 
+# set each node as the head (clean slate)
+# once we hit node.next == None --> break loop
+
+# we return the head of the list
+
+class Node(object):
+	"""Create node."""
+
+	def __init__(self, data, next=None):
+		self.data = data
+		self.next = next
+
+	def as_string(self):
+		"""Print node and all it's successors as a string."""
+
+		out = []
+		n = self
+
+		while n:
+			out.append(str(n.data))
+			n = n.next
+
+		return out
+
+
+
